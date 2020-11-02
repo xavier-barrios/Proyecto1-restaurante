282886623a513b24abb59342c059248b341a0ad2
Victormg42 <71655048+Victormg42@users.noreply.github.com>
2020-11-02 19:39:18 +0100

    Update connection.php

--- model/connection.php
+++ model/connection.php
@@ -1,10 +1,11 @@
 <?php
 include 'config.php';
+// Connectar-nos a la BBDD
 try {
     $dsn = "mysql:host=".SERVIDOR.";dbname=".BD;
-    $pdo = new PDO($dsn, USER, PASSWORD); 
+    $pdo = new PDO($dsn, USER, PASSWORD); // Quan volguem fer connexió a la BBDD, fem servir $pdo
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    //echo "<a href='logoutController.php'>Cerrar sesión</a>";
+    // echo "<a href='logoutController.php'>Cerrar sesión</a>";
 } catch (PDOException $e){
     echo $e->getMessage();
 }
