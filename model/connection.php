8027cdb68d3f722b5a4cf6b476c8a0c55da9dd70
xavier barrios rodriguez <100005770.joan23@fje.edu>
2020-11-02 16:51:43 +0100

    xavi

--- model/connection.php
+++ model/connection.php
@@ -2,9 +2,10 @@
 include 'config.php';
 try {
     $dsn = "mysql:host=".SERVIDOR.";dbname=".BD;
-    $pdo = new PDO($dsn, USER, PASSWORD);
-    // echo "<script>alert('conexion establecida')</script>";
-} catch (PDOException $a){
-    echo $a->getMessage();
+    $pdo = new PDO($dsn, USER, PASSWORD); 
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "<a href='logoutController.php'>Cerrar sesión</a>";
+} catch (PDOException $e){
+    echo $e->getMessage();
 }
 ?>
