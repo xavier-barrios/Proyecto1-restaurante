e151e68af9aa9b955b1f74d3c09236971d52ddfa
“Judit” <juditcava@hotmail.com>
2020-11-02 17:32:46 +0100

    Fichero actualizado

--- controller/admin2Controller.php
+++ controller/admin2Controller.php
@@ -7,6 +7,13 @@ $actualizar = $_GET['act'];
 echo "id_mesa: {$id_mesa}";
 echo "id_mesa: {$actualizar}";
 
+// Información sobre la mesa y la sala a la que pertenece
+$query2 = "SELECT * FROM mesa WHERE id_mesa = $id_mesa";
+$sentencia2=$pdo->prepare($query2);
+$sentencia2->execute();
+$mesa=$sentencia2->fetch(PDO::FETCH_ASSOC);
+print_r($mesa);
+
 if($actualizar == 'Liberar') {
     // Está ocupada (cuando le demos a liberar, tiene que guardarse en histórico)
     try {
@@ -14,15 +21,30 @@ if($actualizar == 'Liberar') {
         $pdo->beginTransaction();
         // OJO
         // tabla historico
+        // $query="INSERT INTO `historico` (`id_historico`, `id_mesa`, `id_sala`, `sillas_mesa`, `fecha_inicio`, `fecha_fin`, `id_usuario`) VALUES (NULL, '1', '2', '6', '2020-11-02 16:27:37', current_timestamp(), '1');";
+        $query="INSERT INTO `historico` (`id_historico`, `id_mesa`, `id_sala`, `sillas_mesa`, `fecha_inicio`, `fecha_fin`, `id_usuario`) VALUES (NULL, ?, ?, ?, ?, current_timestamp(), ?);";
+        $query=$pdo->prepare($query);
+        $id_mesa=$mesa['id_mesa'];
+        $id_sala=$mesa['id_sala'];
+        $sillas_mesa=$mesa['sillas_mesa'];
+        $fecha_inicio=$mesa['fecha_inicio'];
+        $id_usuario=$mesa['id_usuario'];
+        $query->bindParam(1,$id_mesa);
+        $query->bindParam(2,$id_sala);
+        $query->bindParam(3,$sillas_mesa);
+        $query->bindParam(4,$fecha_inicio);
+        $query->bindParam(5,$id_usuario);
+        $id_historico = $pdo->lastInsertId();
+        $query->execute();
         // FIN OJO
         // OJO (viene por la sesión)
         $id_usuario=1;
         // FIN OJO
         // tabla mesa
-        $query = "UPDATE mesa SET fecha_inicio=NULL, id_usuario=NULL WHERE id_mesa = $id_mesa";
-        $sentencia=$pdo->prepare($query);
-        $sentencia->bindParam(1,$id_usuario);
-        $sentencia->execute();
+        $query1="UPDATE mesa SET fecha_inicio=NULL, id_usuario=NULL WHERE id_mesa = $id_mesa;";
+        $sentencia1=$pdo->prepare($query1);
+        $sentencia1->bindParam(1,$id_usuario);
+        $sentencia1->execute();
         $pdo->commit();
         header('Location:../view/admin_2.php');
     } catch (Exception $ex) {
