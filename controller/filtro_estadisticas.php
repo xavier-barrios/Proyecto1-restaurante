5c8ff669f17f3c3135c82b82745434af25ecf3c2
CarlosPiedras <capouca2000@gmail.com>
2020-11-02 19:31:41 +0100

    modificacion

--- controller/filtro_estadisticas.php
+++ controller/filtro_estadisticas.php
@@ -6,17 +6,56 @@ session_start();
 if (isset($_POST['filtro'])){
   $sala = $_POST['sala'];
   $mesa = $_POST['mesa'];
-  $query="SELECT sala.nombre as Sala, mesa.numero_mesa as Mesa from sala inner join mesa on sala.id_sala=mesa.id_sala";
+  $query="SELECT sala.nombre, mesa.numero_mesa, historico.fecha_inicio, historico.fecha_fin FROM historico INNER JOIN mesa ON historico.id_mesa = mesa.id_mesa INNER JOIN sala ON mesa.id_sala = sala.id_sala";
 }else {
-  $query="SELECT * FROM tbl_alumnos";
+  $query="SELECT sala.nombre, mesa.numero_mesa, historico.fecha_inicio, historico.fecha_fin FROM historico INNER JOIN mesa ON historico.id_mesa = mesa.id_mesa INNER JOIN sala ON mesa.id_sala = sala.id_sala WHERE sala.nombre LIKE '%".$sala."%' AND mesa.numero_mesa = $mesa";
+}
+
+$sentencia=$pdo->prepare($query);
+$sentencia->execute();
+$lista_historico=$sentencia->fetchAll(PDO::FETCH_ASSOC);
+// print_r($lista_historico);
+?>
+
+<form method="POST">
+  <h1>FILTARAR</h1>
+  <p>Sala: <input type="text" name="sala" size="40"></p>
+  <p>Mesa: <input type="number" name="mesa" min="1" max="5"></p>
+  
+  <input type="submit" value="Filtrar" name="filtro">
+</form>
+
+
+
+<table>
+  <thead>
+    <tr>
+      <th>Sala</th>
+      <th>Mesa</th>
+      <th>Fecha Inicio</th>
+      <th>Fecha Fin</th>
+    </tr>
+  </thead>
+  <tbody>
+
+<?php
+
+foreach($lista_historico as $historico) {
+  //$id=$persona['id_alumno'];
+  ?>
+
+  <tr>
+    <td><?php echo "{$historico['nombre']}";?></td>
+    <td><?php echo "{$historico['numero_mesa']}"; ?></td>
+    <td><?php echo "{$historico['fecha_inicio']} "; ?></td>
+    <td><?php echo "{$historico['fecha_fin']} "; ?></td>
+  </tr>
+
+<?php
+    
 }
 
 ?>
 
-<form method="POST">
-    <h1>FILTARAR</h1>
-    <p>Sala: <input type="text" name="sala" size="40"></p>
-    <p>Mesa: <input type="number" name="mesa" min="1" max="5"></p>
-    
-    <input type="submit" value="Filtrar" name="filtro">
-</form>
+  </tbody>
+</table>
