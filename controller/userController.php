220657c0f64e5628af348f1cfc039b192e2e6bf5
Victormg42 <100005779.joan23@fje.edu>
2020-11-05 18:27:17 +0100

    Cambios

--- controller/userController.php
+++ controller/userController.php
@@ -5,7 +5,7 @@ require_once '../controller/sessionController.php';
 $puesto_trabajo = $_SESSION['user']->getPuesto_trabajo();
 
 if ($puesto_trabajo == 'camarero') {
-    header('Location: ../view/admin1.php');
+    header('Location: ../view/admin_1.php');
 } else if ($puesto_trabajo == 'mantenimiento') {
     header('Location: ../view/adminMantenimiento.php');
 } else {
